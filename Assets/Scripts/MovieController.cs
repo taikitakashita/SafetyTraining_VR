5e128fa9f2096769f21ca3e664d37278c15b2411
taiki.takashita <taiki.takashita@qdenbs.com>
2019-03-04 18:32:01 +0900

    ・初めてのコミット

---
+++ Assets/Scripts/MovieController.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Video;
+
+public class MovieController : MonoBehaviour
+{
+
+    [SerializeField]
+    GameObject m_menuCanvas;
+
+    [SerializeField]
+    GameObject m_laserPointer;
+
+    [SerializeField]
+    VideoPlayer m_videoPlayer;
+
+    private bool m_menuCanvasStatus;
+
+    // Use this for initialization
+    void Start()
+    {
+        m_menuCanvas.SetActive(false);
+        m_laserPointer.SetActive(false);
+        m_menuCanvasStatus = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (OVRInput.Get(OVRInput.RawButton.Back) && m_menuCanvasStatus == true)
+        {
+            m_videoPlayer.Play();
+            m_menuCanvas.SetActive(false);
+            m_laserPointer.SetActive(false);
+            m_menuCanvasStatus = false;
+        }
+        else if (OVRInput.Get(OVRInput.RawButton.Back) && m_menuCanvasStatus == false)
+        {
+            m_videoPlayer.Pause();
+            m_menuCanvas.SetActive(true);
+            m_laserPointer.SetActive(true);
+            m_menuCanvasStatus = true;
+        }
+    }
+}
---
+++ Assets/Scripts/SwitchScene.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SwitchScene : MonoBehaviour
+{
+    public void SwitchHomeScene()
+    {
+        SceneManager.LoadScene("HomeScene");
+    }
+
+    public void SwitchContentsScene1()
+    {
+        SceneManager.LoadScene("ContentsScene1");
+    }
+
+    public void SwitchContentsScene2()
+    {
+        SceneManager.LoadScene("ContentsScene2");
+    }
+}
